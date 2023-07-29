ce8bc9194ae99675f9c655332fa62299b9800d0d
KyryloTheRylo <kiril.mosiychuk@gmail.com>
2023-07-29 18:57:46 +0300

    Rearanged .rs Files in best structure. Creating order_matching_engine directory with engine.rs in it

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-mod engine;
-use engine::{Price, Limit, Order, BidOrAsk, OrderBook};
+mod orderbook;
+
+use orderbook::{Price, Limit, Order, BidOrAsk, OrderBook};
+mod order_matching_engine::engine;
+
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -19,13 +22,13 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let sell_a: Price = engine::Price::new(15.5);
+    let sell_a: Price = orderbook::Price::new(15.5);
     let lim: Limit = Limit::new(sell_a); 
     let sell_ord: Order = Order::new(5.5, BidOrAsk::Ask);
     let mut book: OrderBook = OrderBook::new();
     book.add_order(15.5, sell_ord ); 
 
-    let buy_a: Price = engine::Price::new(10.5);
+    let buy_a: Price = orderbook::Price::new(10.5);
     let lim: Limit = Limit::new(sell_a); 
     let buy_ord: Order = Order::new(5.5, BidOrAsk::Ask);
     book.add_order(10.5, buy_ord ); 
