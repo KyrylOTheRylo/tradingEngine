68965c2ba52ccaa8649d4965b0c04b31d3ba6c0a
KyryloTheRylo <kiril.mosiychuk@gmail.com>
2023-07-29 21:31:27 +0300

    Rearanged .rs Files in best structure. Creating order_matching_engine directory with engine.rs in it

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 mod order_matching_engine;
-use order_matching_engine::orderbook;
 use order_matching_engine::orderbook::{Order, Limit, Price, OrderBook, BidOrAsk};
+use order_matching_engine::engine::{TradingPair, MatchEngine};
 
 
 
@@ -23,16 +23,27 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let sell_a: Price = orderbook::Price::new(15.5);
+    let sell_a: Price = Price::new(15.5);
     let lim: Limit = Limit::new(sell_a); 
     let sell_ord: Order = Order::new(5.5, BidOrAsk::Ask);
     let mut book: OrderBook = OrderBook::new();
     book.add_order(15.5, sell_ord ); 
 
-    let buy_a: Price = orderbook::Price::new(10.5);
+    let buy_a: Price = Price::new(10.5);
     let lim: Limit = Limit::new(sell_a); 
     let buy_ord: Order = Order::new(5.5, BidOrAsk::Ask);
     book.add_order(10.5, buy_ord ); 
+
+    let btc_usd: TradingPair = TradingPair::new(String::from("BTC"),String::from("USD"));
+
+
+    let mut engine: MatchEngine = MatchEngine::new();
+    engine.add_new_market(btc_usd);
+
+    let pair_wrong: TradingPair = TradingPair::new(String::from("USD"),String::from("Uah"));
+    println!("{:?}", engine.place_limit_order(pair_wrong, 10.5, buy_ord));
+
+
 
 
 
