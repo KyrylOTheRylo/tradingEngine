04aa4aae4ec40b9b137eea0edffcc0fd0520a928
KyryloTheRylo <kiril.mosiychuk@gmail.com>
2023-08-02 13:22:40 +0300

    Rearanging files into module folder order_matching_engine (engine.rs, mod.rs, orderbook.rs)

---
+++ src/order_matching_engine/engine.rs
@@ -0,0 +1,44 @@
+use super::orderbook::{OrderBook, Order};
+use std::collections::HashMap;
+
+#[derive(Debug, Hash, Eq, PartialEq, Clone)]
+pub struct TradingPair {
+    base: String,
+    quote: String,
+}
+ impl TradingPair {
+    pub fn new(base: String, quote: String) -> TradingPair{
+        TradingPair{base, quote}
+    }
+    pub fn to_string(&self) -> String{
+        format!("{}_{}", self.base, self.quote)
+    }
+ }
+ #[derive(Debug)]
+pub struct MatchEngine {
+    orderbooks: HashMap<TradingPair, OrderBook>,
+}
+
+impl MatchEngine {
+ pub fn new() -> MatchEngine{ 
+    MatchEngine{orderbooks : HashMap::new()}   
+}
+
+ pub fn add_new_market(&mut self, pair: TradingPair) {
+
+    self.orderbooks.insert(pair.clone(), OrderBook::new());
+    println!("Opening new orderbook {:?}", pair.to_string())
+ }
+ pub fn place_limit_order(&mut self, pair: TradingPair, price: f64, order: Order) -> Result<(), String> {
+    match  self.orderbooks.get_mut(&pair) {
+        Some(orderbook) => {
+            orderbook.add_order(price, order);
+            Ok(())
+        }
+        None => {
+            Err(format!("the orderbook {} doesn't exist ",  pair.to_string()))
+        }
+        
+    }
+ }
+ }
---
+++ src/order_matching_engine/orderbook.rs
@@ -0,0 +1,116 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct OrderBook {
+    asks: HashMap<Price, Limit>,
+    bids: HashMap<Price, Limit>,
+}
+
+impl OrderBook {
+    pub fn new() -> OrderBook {
+        OrderBook {
+            asks: HashMap::new(),
+            bids: HashMap::new(),
+        }}
+
+    fn add_order_from_price_in_bids_or_asks(&mut self, price: Price, order: Order, bid_or_ask: BidOrAsk)  {
+        let limit_map: &mut HashMap<Price, Limit> = match bid_or_ask {
+            BidOrAsk::Bid => &mut self.bids,
+            BidOrAsk::Ask => &mut self.asks,
+        };
+
+        match limit_map.get_mut(&price) {
+            Some(limit) => {
+                limit.add_order(order)
+            }
+            None => {
+                let mut lim: Limit = Limit::new(price);
+                lim.add_order(order);
+                limit_map.insert(price, lim);
+            }
+        }
+    }
+    pub fn add_order(&mut self, price: f64, order: Order) {
+        let price = Price::new(price);
+        match order.bid_or_ask {
+            BidOrAsk::Ask => {
+                self.add_order_from_price_in_bids_or_asks(price, order, BidOrAsk::Ask);
+            }
+            BidOrAsk::Bid => {
+                self.add_order_from_price_in_bids_or_asks(price, order, BidOrAsk::Bid);
+            }
+        }
+    
+}}
+
+#[derive(Debug, Hash, Eq, PartialEq, Clone, Copy)]
+pub struct Price {
+    mantica: u64,
+    integral:  u64,
+    fractional: u64, 
+
+    
+}
+#[derive(Debug, Hash, Clone, Copy)]
+pub enum BidOrAsk {
+    Bid,
+    Ask  
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Order {
+    size: f64,
+    bid_or_ask: BidOrAsk, 
+    
+}
+
+impl Order {
+    pub fn new(size: f64, bid_or_ask: BidOrAsk) -> Order {
+        Order {
+            size: size,
+            bid_or_ask: bid_or_ask
+        }
+}}
+
+impl Price {
+
+    pub fn new( price: f64) -> Price { 
+        let mantica: u64= 1000000000u64;
+        let integral: u64= price as u64;
+        let fractional: u64 = ((price % 1.0) * mantica as f64) as u64;
+        Price{
+            mantica,
+            integral,
+            fractional,
+        }
+        
+     }
+}
+
+#[derive(Debug)]
+pub struct Limit {
+    price: Price,
+    orders: Vec<Order>,
+
+}
+
+impl Limit {        
+    pub fn new(price: Price) -> Limit { 
+        Limit{
+            price,
+            orders: Vec::new(), 
+        }}
+
+    pub fn fill_order(&mut self, market_order: Order) {
+        for limit_order in self.orders.iter_mut() {
+            match market_order.size  >= limit_order.size {
+                true => {},
+                false => {}
+            }
+    }}
+    pub fn add_order(&mut self, order: Order) {
+        self.orders.push(order);
+        
+    }
+
+}
